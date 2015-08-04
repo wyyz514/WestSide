d6b14274e141be1d89ba5c63c2a9abf5e061f80b
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-04 19:27:00 -0400

    Testing

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 var http = require("http").createServer(handler);
 var io = require("socket.io")(http);
 var port = process.env.PORT||3000;
-var handler = function(rq,rs)
+
+function handler(rq,rs)
 {
   rs.writeHead({"Content-type":"text/html"});
   rs.send("Hello World");
