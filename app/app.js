6d2915e7f6910b5a372e257c35034ccfb190ec1a
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-04 19:38:50 -0400

    Testing 2

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 var http = require("http");
-var io = require("socket.io")(http);
 var port = process.env.PORT||3000;
 
-http.createServer(function (rq,rs){
-  rs.writeHead({"Content-type":"text/html"});
-  rs.end("Hello World");
+http
+.createServer(function(req,res){
+  res.writeHead({"Content-Type":"text/html"});
+  res.end("Hello World");
 })
 .listen(port,function(){
   console.log("Listening on port",port);
