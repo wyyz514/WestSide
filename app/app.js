082b77c0b1e5c1173d342fad98248d05d8c2990a
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-04 19:32:50 -0400

    Fixing syntax issue

--- app/app.js
+++ app/app.js
@@ -1,13 +1,11 @@
-var http = require("http").createServer(handler);
+var http = require("http");
 var io = require("socket.io")(http);
 var port = process.env.PORT||3000;
 
-function handler(rq,rs)
-{
+http.createServer(function (rq,rs){
   rs.writeHead({"Content-type":"text/html"});
-  rs.send("Hello World");
-}
-
-http.listen(port,function(){
+  rs.end("Hello World");
+})
+.listen(port,function(){
   console.log("Listening on port",port);
 });
