31e24b2a667fdc41dedde5be7c295c796e38ec33
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-04 20:12:12 -0400

    Trying to resolve error

--- app/app.js
+++ app/app.js
@@ -6,12 +6,14 @@ var server = http
 .createServer(function(req,res){
   res.writeHead(200,{"Content-Type":"text/html"});
   res.end("Hello World");
-})
-.listen(port,function(){
-  console.log("Listening on port",port);
 });
 
 io(server);
+
+server.listen(port,function(){
+  console.log("Listening on port",port);
+});
+
 
 io.on("connection",function(socket){
   console.log("A connection");
