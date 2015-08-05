01d03ef501d113794447128e38a0e24a90877373
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-04 20:09:48 -0400

    Trying to work with sockets

--- app/app.js
+++ app/app.js
@@ -1,11 +1,18 @@
 var http = require("http");
+var io = require("socket.io");
 var port = process.env.PORT||3000;
 
-http
+var server = http
 .createServer(function(req,res){
   res.writeHead(200,{"Content-Type":"text/html"});
   res.end("Hello World");
 })
 .listen(port,function(){
   console.log("Listening on port",port);
+});
+
+io(server);
+
+io.on("connection",function(socket){
+  console.log("A connection");
 });
