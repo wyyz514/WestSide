ca2d420ce3ad0783fa1ff4a01d4f0a7631d58124
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-04 21:01:01 -0400

    Setting up express

--- app/app.js
+++ app/app.js
@@ -1,19 +1,20 @@
-var http = require("http");
-var io = require("socket.io");
+var express = require("express")
+var app = express();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 var port = process.env.PORT||3000;
-
-var server = http
-.createServer(function(req,res){
-  res.writeHead(200,{"Content-Type":"text/html"});
-  res.end("Hello World");
-});
-
-io(server);
+var path = require("path");
+app.set("view engine","jade")
+app.set("views",path.join(__dirname,"/views"));
+app.use("js",express.static(__dirname+"/js"));
 
 server.listen(port,function(){
   console.log("Listening on port",port);
 });
 
+app.get("/",function(req,res){
+  res.render("index.jade");
+});
 
 io.on("connection",function(socket){
   console.log("A connection");
