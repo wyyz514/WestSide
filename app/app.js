a8b402dfceb519f156d631dab8d39c3430992ea7
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-05 16:37:55 -0400

    Working on SoundCloud auth

--- app/app.js
+++ app/app.js
@@ -14,11 +14,36 @@ server.listen(port,function(){
   console.log("Listening on port",port);
 });
 
-app.get("/",function(req,res){
-  res.render("index.jade",{scripts:["js/ws.js"]});
+app.get("/",function(req,res,next){
+  console.log("Authorizing...(1)");
+  var scAuth = SC.getConnectUrl();
+  res.writeHead(301,{Location:scAuth});
+  res.end();
+  next();
+  //res.render("index.jade",{scripts:["js/ws.js"]});
+});
+
+app.use(function(req,res){
+  var code = req.query.code;
+  SC.authorize(code,function(err,accessToken){
+    if(err)
+    {
+      console.log(err);
+    }
+    else
+    {
+      console.log(accessToken);
+    }
+  });
 });
 
 io.on("connection",function(socket){
   var user = new User(socket,"User");
   console.log(user.getNickName(),"has connected.");
+});
+
+SC.init({
+  id:"34b370aa58ea274d0480fdd2fe51722a",
+  secret:"97708910783570592a82d0a37f462f57",
+  uri:"https://dry-tor-1298.herokuapp.com/"
 });
