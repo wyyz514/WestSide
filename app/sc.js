b43a3ea8e6db8cc3924dd73988b9edb651490249
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-05 19:01:24 -0400

    (Files Changed)-Heroku reload

--- app/sc.js
+++ app/sc.js
@@ -4,7 +4,7 @@ var request = require("request");
 var sc = (function(){
   var soundcloudUrl = "https://soundcloud.com/connect";
   var clientID = "34b370aa58ea274d0480fdd2fe51722a";
-  var redirect = "http://localhost:5000/success";
+  var redirect = "http://127.0.0.1:5000/success";
   function showAuthPopup()
   {
     request.get(soundcloudUrl+"?client_id="+clientID+"&redirect_uri="+redirect,function(err,resp){
