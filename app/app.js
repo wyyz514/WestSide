29bc94440311b35c70702bc3393becf384c677b2
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-05 17:55:14 -0400

    Starting over

--- app/app.js
+++ app/app.js
@@ -1,12 +1,10 @@
 var express = require("express");
 var app = express();
 var http = require("http");
-var https = require("https");
 var server = http.Server(app);
 var io = require("socket.io")(server);
 var port = process.env.PORT||3000;
 var path = require("path");
-var SC = require("machinepack-soundcloud");
 var User = require("./user.js");
 
 app.set("view engine","jade");
@@ -16,20 +14,3 @@ server.listen(port,function(){
   console.log("Listening on port",port);
 });
 
-SC.getLoginUrl({
-  clientId:"34b370aa58ea274d0480fdd2fe51722a",
-  callbackUrl:"localhost:5000/success",
-  display:"popup",
-  responseType:['code']
-})
-.exec({
-  error:function(err){
-    console.log(err);
-  },
-  success:function(url)
-  {
-    https.get(url,function(err,data){
-      console.log(data);
-    });
-  }
-});
