d275199c0bc1387ce932c0d6718f826d9fa24b04
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-06 10:57:52 -0400

    (Files changed) - Heroku reload

--- app/public/js/ws.js
+++ app/public/js/ws.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded",function(){
-  var socket = io.connect("127.0.0.1:5000/success");
+  var socket = io.connect("127.0.0.1:5000");
   socket.emit("connection");
 });
 
