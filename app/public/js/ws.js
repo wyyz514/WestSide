cd81ce1deb652ecac4d924de6a3b203d238fbfff
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-05 23:11:47 -0400

    (Files Changed)-Heroku reload

--- app/public/js/ws.js
+++ app/public/js/ws.js
@@ -1,4 +1,4 @@
-var socket = io.connect("http://localhost");
+var socket = io.connect("http://localhost:5000");
 socket.emit("connection");
 socket.on("authenticated",function(msg){
   console.log(msg.user);
