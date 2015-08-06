a931d46ae746491d5f075245d3276bb857be95a7
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-06 13:48:04 -0400

    (Files changed) - Heroku reload

--- app/sc.js
+++ app/sc.js
@@ -1,11 +1,13 @@
-var request = require("request");
+var https = require("https");
+var qs = require("qs");
 var sc = (function(){
-  var config = {
+  var consts = {
     soundcloudUrl:"https://soundcloud.com/connect",
     clientID:"34b370aa58ea274d0480fdd2fe51722a",
     redirect:"http://127.0.0.1:5000/success",
     responseType:"code",
-    oAuth:"https://api.soundcloud.com/oauth2/token",
+    API:"https://api.soundcloud.com/",
+    oAuth:"oauth2/token",
     secret:"97708910783570592a82d0a37f462f57"
   }
   
@@ -16,47 +18,30 @@ var sc = (function(){
   
   function getToken(code)
   {
-  request.post({url:config.oAuth,
-      oauth:{
-        client_id:config.clientID,
-        redirect_uri:config.redirect,
-        grant_type:'authorization_code',
-        client_secret:config.secret,
-        code:code
-      }},
-      function(err,response,body){
-        if(err)
-        {
-          console.log(error.body);
-        }
-        else
-        {
-          console.log(response);
-        }
-      }
-    );
-      
-  }
-  
-  function makeRequest(token)
-  {
-    var url = "https://api.soundcloud.com/me?oauth_token="+token;
-    request.get(url,function(err,data){
-      if(err)
-      {
-        console.log(err);
-      }
-      else
-      {
-        console.log(data);
-      }
+    var oAuth = {
+      client_id:consts.clientID,
+      redirect_uri:consts.redirect,
+      code:code,
+      client_secret:consts.secret,
+      grant_type:'authorization_code'
+    }
+    
+    var options = {
+      hostname:consts.API,
+      path:consts.oAuth,
+      method:"POST",
+      qs:oAuth
+    };
+    
+    var req = https.request(oAuth,function(res){
+      console.log(res);
     });
+    req.end();
   }
   
   return {
     getConnectUrl:getConnectUrl,
-    getToken:getToken,
-    makeRequest:makeRequest
+    getToken:getToken
   };
 })();
 
