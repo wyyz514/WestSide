9b651fb6fbee8975c1984abd8eb6afacede3e49b
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-06 15:56:36 -0400

    (Files changed) - Heroku reload

--- app/public/js/ws.js
+++ app/public/js/ws.js
@@ -1,4 +1,6 @@
 window.addEventListener("load",function(){
+  if(location.pathname.search("success") < 0)
+    return;
   var socket = io();
   if(document.querySelector("input[type='hidden']").value)
     socket.emit("authenticated",{code:document.querySelector("input[type='hidden']").value});
