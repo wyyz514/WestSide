7de1fd1676afee15df5395c7c265486b9f126a29
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-15 00:34:55 -0400

    Building a queue manager

--- app/public/js/plyr.js
+++ app/public/js/plyr.js
@@ -3,25 +3,37 @@ window.addEventListener("load",function(){
   var queue = [];
   socket.emit("player");
   socket.on("tracks",function(msg){
-    console.log("Tracks");
     queue = JSON.parse(msg.queue);
-    PlayerManager.updateQ(queue);
-    PlayerManager.init();
+    PlayerManager.queue = queue;
+    PlayerManager.renderQueue();
+    PlayerManager.pollSong();
   });
 
   socket.on("sync",function(msg){
-    console.log("Tracks");
     queue = JSON.parse(msg.queue);
-    PlayerManager.updateQ(queue);
+    PlayerManager.renderQueue(diff(PlayerManager.queue,queue));
   });
   
-  var PlayerManager = (function(socket){
-    var isPlaying = false;
-    function updateQ(queue)
+  function diff(q1,q2)
+  {
+    var difference = [];
+    var q1size = q1.length;
+    var q2size = q2.length;
+    var discr = q2size - q1size;
+    console.log("First in local:",q1[q1size - 1]);
+    console.log("First in remote:",q2[q2size - 1]);
+
+    if(discr > 0)
     {
-      this.queue = queue;
-      console.log(this.queue);
-    }
+      for(var index = discr - 1; index >= 0; index--)
+      {
+        difference.push(q2[index]);
+      }
+    }  
+    return difference;
+  }
+  
+  var PlayerManager = (function(socket){
     
     function getQueue()
     {
@@ -37,6 +49,8 @@ window.addEventListener("load",function(){
     
     function init()
     {
+      var parent = document.querySelector(".ws-player-queue");
+      parent.removeChild(parent.firstElementChild);
       var urlBase = "https://w.soundcloud.com/player/?url=";
       if(this.queue.length === 0)
         return;
@@ -48,24 +62,83 @@ window.addEventListener("load",function(){
         this.widget = SC.Widget(iframe);
         document.body.appendChild(iframe);
         this.widget.bind(SC.Widget.Events.FINISH,function(){
-          PlayerManager.init();
+          pollSong();
         });
       }
       else
       {
         this.widget.load(this.queue.pop().link,{auto_play:true});
-        console.log(getIframe().src);
       }
       socket.emit("dequeue");
       return this.widget;
     }
     
+    function renderQueue()
+    {
+      var args = arguments[0];
+      if(args
+      && Object.prototype.toString.call(args) == "[object Array]")
+      {
+        var len = args.length;
+        for(var index = 0; index < len; index++)
+        {
+          (function(obj){
+            var song = args.pop();
+            obj.queue.unshift(song);
+            renderRow(song);
+          })(this);
+        }
+      }
+      else
+      {
+        var qlength = PlayerManager.queue.length;
+        for(var index = qlength - 1; index >=0; index--)
+        {
+          (function(i){
+            renderRow(PlayerManager.queue[i]);
+          })(index);
+        }
+      }
+    }
+    
+    function renderRow(song)
+    {
+      var parent = document.querySelector(".ws-player-queue");
+      var songRow = document.createElement("div");
+      songRow.classList.add("ws-player-song-row");
+      var songInfo = document.createElement("div");
+      songInfo.classList.add("ws-player-song-info");
+      var songName = document.createElement("div");
+      songName.classList.add("ws-player-song-name");
+      var songArtist = document.createElement("div");
+      songArtist.classList.add("ws-player-song-artist");
+      songName.innerText = song.title;
+      songArtist.innerText = song.artist;
+      songInfo.appendChild(songName);
+      songInfo.appendChild(songArtist);
+      songRow.appendChild(songInfo);
+      parent.appendChild(songRow);
+    }
+    
+    function pollSong()
+    {
+      var poll = setInterval(function(){
+        if(queue.length > 0)
+        {
+          PlayerManager.init();
+          clearInterval(poll);
+        }
+      },3000);
+    }
+    
     return {
       init:init,
-      updateQ:updateQ
+      renderQueue:renderQueue,
+      pollSong:pollSong
     }
   })(socket);
   
+  window.PM = PlayerManager;
   PlayerManager.init = PlayerManager.init.bind(PlayerManager);
   
 });
